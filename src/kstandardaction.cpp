74a6f75685e286d54a82e503dbaec934d0252566
Friedrich W. H. Kossebau <kossebau@kde.org>
2017-06-22 12:42:41 +0200

    Remove duplicated KCONFIGWIDGETS_EXPORT from source file, already in header

--- src/kstandardaction.cpp
+++ src/kstandardaction.cpp
@@ -64,7 +64,7 @@ QList<StandardAction> actionIds()
     return result;
 }
 
-KCONFIGWIDGETS_EXPORT KStandardShortcut::StandardShortcut shortcutForActionId(StandardAction id)
+KStandardShortcut::StandardShortcut shortcutForActionId(StandardAction id)
 {
     const KStandardActionInfo *pInfo = infoPtr(id);
     return (pInfo) ? pInfo->idAccel : KStandardShortcut::AccelNone;
