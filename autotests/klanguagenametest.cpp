91077def022a999ce46c91347f1167626266f460
Albert Astals Cid <aacid@kde.org>
2019-01-07 11:42:28 +0100

    Add KLanguageName
    Summary: KLanguageName is a helper namespace that returns the name of a given language code.
    
    Reviewers: apol
    
    Reviewed By: apol
    
    Subscribers: hein, kde-frameworks-devel, sitter, markg, apol
    
    Tags: #frameworks
    
    Differential Revision: https://phabricator.kde.org/D10446

---
+++ autotests/klanguagenametest.cpp
@@ -0,0 +1,92 @@
+/*
+    Copyright (c) 2018 Harald Sitter <sitter@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <QObject>
+#include <QtTest>
+
+#include "klanguagename.h"
+
+static void setEnvironment()
+{
+    qputenv("LOCALE", "C.UTF-8");
+    qputenv("LANG", "en");
+    qputenv("XDG_DATA_DIRS", qUtf8Printable(QFINDTESTDATA("kf5_entry_data")));
+
+    // NOTE
+    // - fr has no translations
+    // - es has no kf5_entry at all
+    // - other languages under testing are complete
+}
+
+class KLanguageNameTest : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void testNameForCode()
+    {
+        // This is somewhat wrong, it should not say US.
+        QCOMPARE(KLanguageName::nameForCode("en"), "US English");
+
+        QCOMPARE(KLanguageName::nameForCode("de"), "German");
+        QCOMPARE(KLanguageName::nameForCode("pt"), "Portuguese");
+        QCOMPARE(KLanguageName::nameForCode("ca"), "Catalan");
+    }
+
+    void testNameForCodeInLocale()
+    {
+        // This is somewhat wrong, it should not say US.
+        QCOMPARE(KLanguageName::nameForCodeInLocale("en", "de"), "US-Englisch");
+
+        QCOMPARE(KLanguageName::nameForCodeInLocale("de", "de"), "Deutsch");
+        QCOMPARE(KLanguageName::nameForCodeInLocale("pt", "de"), "Portugiesisch");
+        QCOMPARE(KLanguageName::nameForCodeInLocale("ca", "de"), "Katalanisch");
+    }
+
+    void testNoTranslation()
+    {
+        // This has an entry file but no translation => QLocale.
+        QCOMPARE(KLanguageName::nameForCode("fr"), "French");
+        QCOMPARE(KLanguageName::nameForCodeInLocale("fr", "de"), "French");
+        // When in the same language, use the native name.
+        QCOMPARE(KLanguageName::nameForCodeInLocale("fr", "fr"), "français");
+    }
+
+    void testNoEntry()
+    {
+        // This has no entry file => QLocale.
+        QCOMPARE(KLanguageName::nameForCode("es"), "Spanish");
+        QCOMPARE(KLanguageName::nameForCodeInLocale("es", "de"), "Spanish");
+        // When in the same language, use the native name.
+        QCOMPARE(KLanguageName::nameForCodeInLocale("es", "es"), "español de España");
+    }
+
+    void testNoString()
+    {
+        // Qt doesn't have za support, we have no test fixture, so no string.
+        QCOMPARE(KLanguageName::nameForCode("za"), QString());
+    }
+};
+
+Q_COREAPP_STARTUP_FUNCTION(setEnvironment)
+
+QTEST_GUILESS_MAIN(KLanguageNameTest)
+
+#include "klanguagenametest.moc"
---
+++ src/klanguagename.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 1999-2003 Hans Petter Bieker <bieker@kde.org>
+ *           (c) 2007      David Jarvie <software@astrojar.org.uk>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
+
+#include "klanguagename.h"
+#include "kconfigwidgets_debug.h"
+
+#include <kconfig.h>
+#include <kconfiggroup.h>
+
+QString KLanguageName::nameForCode(const QString &code)
+{
+    const QStringList parts = QLocale().name().split(QChar('_'));
+    return nameForCodeInLocale(code, parts.at(0));
+}
+
+QString KLanguageName::nameForCodeInLocale(const QString &code, const QString &outputCode)
+{
+    const QString realCode = code == QStringLiteral("en") ? QStringLiteral("en_US") : code;
+    const QString realOutputCode = outputCode == QStringLiteral("en") ? QStringLiteral("en_US") : outputCode;
+
+    const QString entryFile =
+            QStandardPaths::locate(QStandardPaths::GenericDataLocation,
+                                   QStringLiteral("locale") + QLatin1Char('/') + realCode + QStringLiteral("/kf5_entry.desktop"));
+    if (!entryFile.isEmpty()) {
+        KConfig entry(entryFile, KConfig::SimpleConfig);
+        entry.setLocale(realOutputCode);
+        const KConfigGroup group(&entry, "KCM Locale");
+        const QString name = group.readEntry("Name");
+
+        // KConfig doesn't have a way to say it didn't find the entry in
+        // realOutputCode and returned the english version so we check for the
+        // english version, if it's equal to the "non-english" version we'll use
+        // otherwise we defer to QLocale.
+        entry.setLocale("en_US");
+        const QString englishName = group.readEntry("Name");
+        if (name != englishName || realOutputCode == QStringLiteral("en_US")) {
+            return name;
+        }
+    }
+
+    const QLocale locale(realCode);
+    if (locale != QLocale::c()) {
+        if (realCode == realOutputCode) {
+            return locale.nativeLanguageName();
+        }
+        return QLocale::languageToString(locale.language());
+    }
+
+    return QString();
+}
