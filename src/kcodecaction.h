69e066de046857521ba029a05a401ca1f8385ec9
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2020-08-07 21:42:11 +0000

    KConfigWidgets: Convert copyright headers to SPDX expressions

--- src/kcodecaction.h
+++ src/kcodecaction.h
@@ -1,28 +1,9 @@
 /*
-   kcodecaction.h
+    SPDX-FileCopyrightText: 2003 Jason Keirstead <jason@keirstead.org>
+    SPDX-FileCopyrightText: 2003-2006 Michel Hermier <michel.hermier@gmail.com>
+    SPDX-FileCopyrightText: 2007 Nick Shaforostoff <shafff@ukr.net>
 
-    Copyright (c) 2003      Jason Keirstead   <jason@keirstead.org>
-    Copyright (c) 2003-2006 Michel Hermier    <michel.hermier@gmail.com>
-    Copyright (c) 2007      Nick Shaforostoff <shafff@ukr.net>
-
-    ********************************************************************
-    *                                                                  *
-    * This library is free software; you can redistribute it and/or    *
-    * modify it under the terms of the GNU Lesser General Public       *
-    * License as published by the Free Software Foundation; either     *
-    * version 2 of the License, or (at your option) any later version. *
-    *                                                                  *
-    * This library is distributed in the hope that it will be useful,  *
-    * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
-    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
-    * GNU Lesser General Public License for more details.              *
-    *                                                                  *
-    * You should have received a copy of the GNU Lesser General Public *
-    * License along with this library; if not, write to the            *
-    * Free Software Foundation, Inc., 51 Franklin Street,              *
-    * Fifth Floor, Boston, MA  02110-1301  USA                         *
-    *                                                                  *
-    ********************************************************************
+    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 #ifndef KCODECACTION_H
 #define KCODECACTION_H
