527cf1a237dd8aee0f206ea8b4885fa4dd05caa2
Waqar Ahmed <waqar.17a@gmail.com>
2023-03-07 17:54:15 +0500

    CommandBar: Fix lastUsedActions not restored

--- autotests/kcmdbartest.cpp
+++ autotests/kcmdbartest.cpp
@@ -5,13 +5,21 @@
 */
 
 #include <QAbstractItemModel>
+#include <QAbstractItemView>
 #include <QHBoxLayout>
+#include <QLineEdit>
 #include <QListView>
 #include <QMainWindow>
 #include <QObject>
+#include <QStandardPaths>
 #include <QTest>
 
 #include "kcommandbar.h"
+
+static void setEnvironment()
+{
+    QStandardPaths::setTestModeEnabled(true);
+}
 
 class KCommandBarTest : public QObject
 {
@@ -43,7 +51,56 @@ private Q_SLOTS:
         b.show();
         // QTest::qWait(100000);
     }
+
+    void testLastUsedActionRestored()
+    {
+        QMainWindow w;
+        auto central = new QWidget(&w);
+        w.setCentralWidget(central);
+
+        KCommandBar::ActionGroup ag;
+        ag.name = QStringLiteral("Group1");
+        QAction *a = new QAction(QStringLiteral("Act1"), &w);
+        a->setObjectName("act1");
+        ag.actions << a;
+        a = new QAction(QStringLiteral("Act2"), &w);
+        a->setObjectName("act2");
+        ag.actions << a;
+        {
+            KCommandBar b(&w);
+            b.setActions({ag});
+
+            auto treeView = b.findChild<QAbstractItemView *>();
+            auto lineEdit = b.findChild<QLineEdit *>();
+            QVERIFY(treeView);
+            QVERIFY(lineEdit);
+            QVERIFY(treeView->model());
+            QCOMPARE(treeView->model()->rowCount(), 3);
+            QCOMPARE(treeView->model()->index(1, 0).data().toString(), QStringLiteral("Group1: %1").arg(ag.actions[0]->text()));
+            QCOMPARE(treeView->model()->index(2, 0).data().toString(), QStringLiteral("Group1: %1").arg(ag.actions[1]->text()));
+
+            QTest::sendKeyEvent(QTest::KeyAction::Press, treeView, Qt::Key_Down, {}, Qt::NoModifier);
+            QTest::sendKeyEvent(QTest::KeyAction::Press, treeView, Qt::Key_Down, {}, Qt::NoModifier);
+            QCOMPARE(treeView->currentIndex().data().toString(), QStringLiteral("Group1: Act2"));
+
+            lineEdit->returnPressed();
+        }
+
+        {
+            KCommandBar b(&w);
+            b.setActions({ag});
+
+            auto treeView = b.findChild<QAbstractItemView *>();
+            QCOMPARE(treeView->model()->rowCount(), 3);
+            // Act2 is at the top now
+            QCOMPARE(treeView->model()->index(0, 0).data().toString(), QStringLiteral("Group1: %1").arg(ag.actions[1]->text()));
+            // Act1 is at the end
+            QCOMPARE(treeView->model()->index(2, 0).data().toString(), QStringLiteral("Group1: %1").arg(ag.actions[0]->text()));
+        }
+    }
 };
+
+Q_COREAPP_STARTUP_FUNCTION(setEnvironment)
 
 QTEST_MAIN(KCommandBarTest)
 
