b346fa87c3ba7829a59029389b0a359a26a43c78
David Faure <faure@kde.org>
2024-05-05 12:28:49 +0200

    KConfigDialogManager: port to non-deprecated QCheckBox signal
    Seen when opening the "Configure Annotations" dialog from okular
    KConfigDialogManager::parseChildren QObject::connect: Connecting from COMPAT signal (QCheckBox::stateChanged(int))
    using Qt 6.7

--- src/kconfigdialogmanager.cpp
+++ src/kconfigdialogmanager.cpp
@@ -50,8 +50,12 @@ void KConfigDialogManager::initMaps()
     }
 
     if (s_changedMap()->isEmpty()) {
-        // QT
+        // Qt
+#if QT_VERSION >= QT_VERSION_CHECK(6, 7, 0)
+        s_changedMap()->insert(QStringLiteral("QCheckBox"), SIGNAL(checkStateChanged(Qt::CheckState)));
+#else
         s_changedMap()->insert(QStringLiteral("QCheckBox"), SIGNAL(stateChanged(int)));
+#endif
         s_changedMap()->insert(QStringLiteral("QPushButton"), SIGNAL(clicked(bool)));
         s_changedMap()->insert(QStringLiteral("QRadioButton"), SIGNAL(toggled(bool)));
         s_changedMap()->insert(QStringLiteral("QGroupBox"), SIGNAL(toggled(bool)));
